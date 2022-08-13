fa4b08121a071564d36377c57dd14afc3e68cc4c
Regina Ragnarsdottir <44742460+rragnarsd@users.noreply.github.com>
2022-08-13 09:51:14 +0000

    Delete specific message

--- lib/screens/chat_screen/widgets/chats_widget.dart
+++ lib/screens/chat_screen/widgets/chats_widget.dart
@@ -48,53 +48,60 @@ class ChatsWidget extends StatelessWidget {
                       Timestamp t = message['createdAt'];
                       DateTime parsedDate = t.toDate();
 
-                      return Row(
-                        mainAxisAlignment: currentUser
-                            ? MainAxisAlignment.end
-                            : MainAxisAlignment.start,
-                        children: [
-                          if (!currentUser)
-                            CircleAvatar(
-                              radius: 16,
-                              backgroundColor: Colors.grey.shade400,
-                            ),
-                          const SizedBox(width: 10),
-                          Column(
-                            crossAxisAlignment: currentUser
-                                ? CrossAxisAlignment.end
-                                : CrossAxisAlignment.start,
-                            children: [
-                              Container(
-                                padding: const EdgeInsets.all(10),
-                                constraints:
-                                    const BoxConstraints(maxWidth: 160),
-                                decoration: BoxDecoration(
-                                  color: currentUser
-                                      ? ConstColors.greenCyan
-                                      : Colors.grey.shade300,
-                                  border: Border.all(),
-                                  borderRadius: BorderRadius.only(
-                                    topLeft: const Radius.circular(13),
-                                    topRight: const Radius.circular(13),
-                                    bottomLeft: currentUser
-                                        ? const Radius.circular(13)
-                                        : Radius.zero,
-                                    bottomRight: !currentUser
-                                        ? const Radius.circular(13)
-                                        : Radius.zero,
-                                  ),
-                                ),
-                                child: Text(message['message']),
+                      return InkWell(
+                        splashColor: Colors.transparent,
+                        focusColor: Colors.transparent,
+                        onDoubleTap: () {
+                          FirebaseService().deleteChats(message.id);
+                        },
+                        child: Row(
+                          mainAxisAlignment: currentUser
+                              ? MainAxisAlignment.end
+                              : MainAxisAlignment.start,
+                          children: [
+                            if (!currentUser)
+                              CircleAvatar(
+                                radius: 16,
+                                backgroundColor: Colors.grey.shade400,
                               ),
-                              const SizedBox(height: 5),
-                              Text(
-                                Functions().convertToAgo(parsedDate, context),
-                                style:
-                                    TextStyles.style12.copyWith(fontSize: 10),
-                              )
-                            ],
-                          ),
-                        ],
+                            const SizedBox(width: 10),
+                            Column(
+                              crossAxisAlignment: currentUser
+                                  ? CrossAxisAlignment.end
+                                  : CrossAxisAlignment.start,
+                              children: [
+                                Container(
+                                  padding: const EdgeInsets.all(10),
+                                  constraints:
+                                      const BoxConstraints(maxWidth: 160),
+                                  decoration: BoxDecoration(
+                                    color: currentUser
+                                        ? ConstColors.greenCyan
+                                        : Colors.grey.shade300,
+                                    border: Border.all(),
+                                    borderRadius: BorderRadius.only(
+                                      topLeft: const Radius.circular(13),
+                                      topRight: const Radius.circular(13),
+                                      bottomLeft: currentUser
+                                          ? const Radius.circular(13)
+                                          : Radius.zero,
+                                      bottomRight: !currentUser
+                                          ? const Radius.circular(13)
+                                          : Radius.zero,
+                                    ),
+                                  ),
+                                  child: Text(message['message']),
+                                ),
+                                const SizedBox(height: 5),
+                                Text(
+                                  Functions().convertToAgo(parsedDate, context),
+                                  style:
+                                      TextStyles.style12.copyWith(fontSize: 10),
+                                )
+                              ],
+                            ),
+                          ],
+                        ),
                       );
                     } else {
                       return Container();
